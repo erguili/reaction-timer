2f9b171a35c8cb5da22f758cbe249e7265a71da3
Ilyas <zone_ender1994@hotmail.com>
2022-09-15 11:44:36 +0200

    First commit

---
+++ src/components/Results.vue
@@ -0,0 +1,39 @@
+<template>
+<p>Reaction Time {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        this.score < 350 ? 
+            this.rank = 'You are fast!!'
+         :
+        this.score < 600 ?  
+            this.rank = 'Not bad, but keep trying.'
+         : 
+            this.rank = 'Take your time grandpa.'
+        console.log(this.rank)
+    }
+    // mounted() {
+    //     if (this.score < 350) {
+    //         this.rank = 'You are fast!!'
+    //     } else if (this.score < 450) {
+    //         this.rank = 'Not bad, keep trying.'
+    //     }
+    //     else {
+    //         this.rank = 'Take your time grandpa.'
+    //     }console.log(this.rank)
+    // }
+}
+
+</script>
+
+<style>
+
+</style>
